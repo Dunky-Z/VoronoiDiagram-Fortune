45d558ff29a8aece10460d00850a41db6b19bd25
如云泊 <254758318@qq.com>
2019-04-14 22:33:40 +0800

    添加项目文件。

---
+++ VoronoiDiagram-Fortune/intersectionArray.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Box.h"
+
+struct intersectionArray {
+	Box::Intersection *first;
+	Box::Intersection *second;
+
+	// Constructor and Destructor
+	intersectionArray();
+	~intersectionArray();
+
+	// Operators
+	Box::Intersection* operator[](unsigned int i);
+
+	// Operations
+	void swap();
+};
